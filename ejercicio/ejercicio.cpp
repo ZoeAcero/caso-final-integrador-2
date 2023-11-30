4e4bbc413f0d945911047292c8fc7272ba3d4ef5
ZoeAcero <zar.amazona@gmail.com>
2023-11-30 12:01:28 +0100

    2

--- ejercicio/ejercicio.cpp
+++ ejercicio/ejercicio.cpp
@@ -16,3 +16,20 @@ struct ConsoleBox {
     void set_text(const string& text) { cout <<text << endl; }
 
 };
+
+void load_script(const char* filename, bool show_script = false) {
+
+    string script;
+
+    FILE* file = nullptr;
+
+    try {
+        file = fopen(filename, "r");
+        if (file == nullptr) {
+            throw runtime_error("Error al abrir el archivo");
+        }
+
+        int c;
+        char
+    }
+}
